57593fde60fcc9a4cd982b48f035144fed86b9b7
YeisonMauricio28004 <116232628+YeisonMauricio28004@users.noreply.github.com>
2023-03-24 20:54:48 -0600

    Ejercicio de Tarea Semana 9 ya Terminado

---
+++ EjercicioSemana8/EjercicioSemana8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EjercicioSemana8.Models;
+
+Producto producto = new Producto();
+using (AlmacenContext db= new AlmacenContext())
+{
+    Console.WriteLine("Por Favor Digite el Nombre");
+    producto.Nombre = Console.ReadLine();
+    Console.WriteLine("Por Favor Digite La Descripcion");
+    producto.Descripcion = Console.ReadLine();
+    Console.WriteLine("Por Favor Digite El Precio");
+    producto.Precio = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Por Favor Digite el Stock");
+    producto.Stock = int.Parse(Console.ReadLine());
+    db.Add(producto);
+    db.SaveChanges();
+    Console.WriteLine("Su Producto Fue Ingresado con Exito !\n");
+
+    var lista = db.Productos.ToList();
+    foreach(var mostrar in lista)
+    {
+        Console.WriteLine("\nNombre:  " + mostrar.Nombre);
+        Console.WriteLine("Descripcion: " + mostrar.Descripcion);
+        Console.WriteLine("Precio  " + mostrar.Precio);
+        Console.WriteLine("Stock " + mostrar.Stock); 
+    }
+
+}
+
+
